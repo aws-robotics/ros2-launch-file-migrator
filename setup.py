8ce731957d0adb7be3a3fe98be072da476ba6fed
Tim Robinson <tim@amazon.com>
2019-11-22 11:06:51 -0800

    Use argparse, improve setup, add more exceptions
    - Use argparse to make the usage of the migration tool look nicer.
    - Add more information and requires to setup.py instead of requirements
    file.
    - Add better exception handling and aborting to main migrate file

--- setup.py
+++ setup.py
@@ -4,16 +4,27 @@ package_name = "ros2_launch_file_migrator"
 
 setup(
   name=package_name,
-  version="0.3",
+  version="0.4",
   description="Migrates a ROS1 XML launch file into a ROS2 Python launch file",
-  url="",
+  url="https://github.com/aws-robotics/ros2-launch-file-migrator/",
   author="AWS RoboMaker",
   author_email="ros-contributions@amazon.com",
-  license="MIT",
+  maintainer="AWS RoboMaker",
+  maintainer_email="ros-contributions@amazon.com",
+  keywords=['ROS', 'ROS2'],
+  license="Apache License, Version 2.0",
   packages=find_packages(),
   package_data={
     "ros2_launch_file_migrator": ["templates/*"]
   },
+  install_requires=[
+    'jinja2',
+    'autopep8'
+  ],
+  tests_require=[
+    'pytest'
+  ],
   scripts=[package_name + "/bin/migrate_launch_file"],
-  zip_save=True
+  zip_save=True,
+  python_requires='>=3.6'
 )
